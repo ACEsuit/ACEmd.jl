51b1acec7ea48887090691ec2f22fb033843be4a
Documenter.jl <documenter@juliadocs.github.io>
2023-03-20 14:25:23 +0000

    build based on c25dbe3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ACEapi","category":"page"},{"location":"#ACEapi","page":"Home","title":"ACEapi","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ACEapi.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ACEapi]","category":"page"}]
+}
