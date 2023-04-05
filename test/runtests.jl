b48b5ccc3b9502aad9b66f8de24a1634fd90ef54
Teemu Järvinen <teemu.j.jarvinen@gmail.com>
2023-04-06 02:39:10 +0300

    add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,20 @@
 using ACEapi
+using ACE1
+#using AtomsBase
+#using ExtXYZ
 using Test
+
+fname_ace = joinpath(pkgdir(ACEapi), "test", "data", "TiAl.json")
+fname_xyz = joinpath(pkgdir(ACEapi), "test", "data", "TiAl.xyz")
 
 @testset "ACEapi.jl" begin
     # Write your tests here.
+    pot = load_ace_model(fname_ace)
+    pot_old = load_ace_model(fname_ace; old_format=true)
+    data = read_extxyz(fname_xyz)[end]
+
+
+    @test ace_energy(pot, data) ≈ ACE1.energy(pot_old, data)
+    @test ace_forces(pot, data) ≈ ACE1.forces(pot_old, data)
+    @test ace_virial(pot, data) ≈ ACE1.virial(pot_old, data)
 end
