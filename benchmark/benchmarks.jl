e6f5bbc6456ed6af84314bd8df863a8fc44bccab
Teemu Järvinen <teemu.j.jarvinen@gmail.com>
2023-05-05 01:02:54 +0300

    updated benchmark structure (#6)

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -4,7 +4,7 @@ using ACE1
 using ExtXYZ
 using AtomsBase
 
-const SUITE = BenchmarkGroup()
+SUITE = BenchmarkGroup()
 
 fname_ace = joinpath(pkgdir(ACEapi), "data", "TiAl.json")
 fname_xyz = joinpath(pkgdir(ACEapi), "data", "TiAl-big.xyz")
@@ -19,23 +19,31 @@ data_huge = FastSystem(ExtXYZ.Atoms(read_frame(fname_xyz_huge)))
 
 
 
-SUITE["JuLIP"] = BenchmarkGroup(["JuLIP","Modes", "Size"])
-SUITE["AtomsBase"] = BenchmarkGroup(["AtomsBase", "Modes", "Size"])
+SUITE["JuLIP"] = BenchmarkGroup([],
+        "energy" => BenchmarkGroup(),
+        "forces" => BenchmarkGroup(),
+        "virial" => BenchmarkGroup()
+)
+SUITE["AtomsBase"] = BenchmarkGroup([],
+        "energy" => BenchmarkGroup(),
+        "forces" => BenchmarkGroup(),
+        "virial" => BenchmarkGroup()
+)
 
-SUITE["JuLIP", "energy", "big"] = @benchmarkable ACE1.energy($pot_julip, $data_julip)
-SUITE["JuLIP", "energy", "huge"] = @benchmarkable ACE1.energy($pot_julip, $data_julip_huge)
-SUITE["AtomsBase", "energy", "big"] = @benchmarkable ace_energy($pot, $data)
-SUITE["AtomsBase", "energy", "huge"] = @benchmarkable ace_energy($pot, $data_huge)
+SUITE["JuLIP"]["energy"]["big"] = @benchmarkable ACE1.energy($pot_julip, $data_julip)
+SUITE["JuLIP"]["energy"]["huge"] = @benchmarkable ACE1.energy($pot_julip, $data_julip_huge)
+SUITE["AtomsBase"]["energy"]["big"] = @benchmarkable ace_energy($pot, $data)
+SUITE["AtomsBase"]["energy"]["huge"] = @benchmarkable ace_energy($pot, $data_huge)
 
-SUITE["JuLIP", "forces", "big"] = @benchmarkable ACE1.forces($pot_julip, $data_julip)
-SUITE["JuLIP", "forces", "huge"] = @benchmarkable ACE1.forces($pot_julip, $data_julip_huge)
-SUITE["AtomsBase", "forces", "big"] = @benchmarkable ace_forces($pot, $data)
-SUITE["AtomsBase", "forces", "huge"] = @benchmarkable ace_forces($pot, $data_huge)
+SUITE["JuLIP"]["forces"]["big"] = @benchmarkable ACE1.forces($pot_julip, $data_julip)
+SUITE["JuLIP"]["forces"]["huge"] = @benchmarkable ACE1.forces($pot_julip, $data_julip_huge)
+SUITE["AtomsBase"]["forces"]["big"] = @benchmarkable ace_forces($pot, $data)
+SUITE["AtomsBase"]["forces"]["huge"] = @benchmarkable ace_forces($pot, $data_huge)
 
-SUITE["JuLIP", "virial", "big"] = @benchmarkable ACE1.virial($pot_julip, $data_julip)
-SUITE["JuLIP", "virial", "huge"] = @benchmarkable ACE1.virial($pot_julip, $data_julip_huge)
-SUITE["AtomsBase", "virial", "big"] = @benchmarkable ace_virial($pot, $data)
-SUITE["AtomsBase", "virial", "huge"] = @benchmarkable ace_virial($pot, $data_huge)
+SUITE["JuLIP"]["virial"]["big"] = @benchmarkable ACE1.virial($pot_julip, $data_julip)
+SUITE["JuLIP"]["virial"]["huge"] = @benchmarkable ACE1.virial($pot_julip, $data_julip_huge)
+SUITE["AtomsBase"]["virial"]["big"] = @benchmarkable ace_virial($pot, $data)
+SUITE["AtomsBase"]["virial"]["huge"] = @benchmarkable ace_virial($pot, $data_huge)
 
 
 
